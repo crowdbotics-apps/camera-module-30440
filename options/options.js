eb91021e58a3549e76e097ea422f480bf8450812
Crowdbotics <team@crowdbotics.com>
2021-09-10 17:33:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "camera_module_30440Identifier",
+  url: "https://camera_module_30440Identifier.botics.co",
+  api: "https://camera_module_30440Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
